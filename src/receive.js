31ca4408bbc276b36461d45d123de102327d52df
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-27 17:16:36 +0800

    feat: add receiver

--- src/receive.js
+++ src/receive.js
@@ -1,28 +1,27 @@
-import amqp from 'amqplib/callback_api'
+import amqp from "amqplib/callback_api";
 
-console.log('connecting', amqp)
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0) {
-        throw error0
+amqp.connect("amqp://localhost", function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    // only creates if it doesn't already exist aka idempotent
+    if (error1) {
+      throw error1;
     }
-    connection.createChannel(function(error1, channel) { // only creates if it doesn't already exist aka idempotent
-      if (error1) {
-          throw error1
-      }
-      const queue = 'hello'
-      const msg = 'hello world'
+    const queue = "hello";
+    channel.assertQueue(queue, {
+      durable: false
+    });
+    console.log("waiting for messages in %s", queue);
 
-      channel.assertQueue(queue, {
-        durable: false
-      });
-  
-      channel.sendToQueue(queue, Buffer.from(msg));
-      console.log(" [x] Sent %s", msg);
-    })
+    channel.consume(
+      queue,
+      function(msg) {
+        console.log("received %s", msg);
+      },
+      { noAck: true }
+    );
+  });
 
-    setTimeout(function() {
-        connection.close()
-        console.log('connection closed')
-        process.exit(0)
-      }, 500)
-})
+});
